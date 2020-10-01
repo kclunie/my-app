edab0a650dd864edff831df5a9adbc3a560609ec
Kristen Clunie <kcluniesa@yahoo.com>
2020-09-30 22:28:38 -0400

    connect app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainContainer from './containers/MainContainer'
+import Nav from './containers/Nav'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
+      <Nav />
+      <MainContainer />
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +24,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
