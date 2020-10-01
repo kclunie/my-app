90be37b4058da9de32ae524ce1e23288dd14f444
Kristen Clunie <kcluniesa@yahoo.com>
2020-09-30 22:31:02 -0400

    add maincontainer

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -41,95 +41,99 @@ export default class MainContainer extends React.Component {
     console.log(this.state.cards)
   }
 
-//   addList = (cardId, input) => {
-//     fetch("http://localhost:3000/lists", {
-//       method: "POST",
-//       headers: {
-//         'Content-Type': 'application/json',
-//         Accept: 'application/json'
-//       },
-//       body: JSON.stringify({
-//         description: input,
-//         card_id: cardId,
-//         completed: false
-//       })
-//     })
-//     .then(resp => resp.json())
-//     .then(newList => {
-//       const foundCard = {...this.state.cards.find(card => card.id === cardId)}
-//       foundCard.lists = [...foundCard.lists, newList]
+  addList = (cardId, input) => {
+    fetch("http://localhost:3000/lists", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        description: input,
+        card_id: cardId,
+        completed: false
+      })
+    })
+    .then(resp => resp.json())
+    .then(newList => {
+      const foundCard = {...this.state.cards.find(card => card.id === cardId)}
+      foundCard.lists = [...foundCard.lists, newList]
 
-//       const newCards = this.state.cards.map(card => {
-//         if (card.id === cardId){
-//           return foundCard
-//         } else {
-//           return card
-//         }
-//       })
+      const newCards = this.state.cards.map(card => {
+        if (card.id === cardId){
+          return foundCard
+        } else {
+          return card
+        }
+      })
 
-//       this.setState({
-//         cards: newCards
-//       })
+      this.setState({
+        cards: newCards
+      })
 
-//     })
-//   }
+    })
+    console.log(this.state.cards)
+  }
 
-//   handleClickList = (cardId, listId) => {
+  handleClickList = (cardId, listId) => {
 
-//     const foundCard = {...this.state.cards.find(card => card.id === cardId)}
-//     const foundList = foundCard.lists.find(list => list.id === listId)
+    const foundCard = {...this.state.cards.find(card => card.id === cardId)}
+    const foundList = foundCard.lists.find(list => list.id === listId)
 
-//     let newState = null
+    let newState = null
 
-//     if (foundList.completed) {
-//       newState = false
-//     } else {
-//       newState = true
-//     }
+    if (foundList.completed) {
+      newState = false
+    } else {
+      newState = true
+    }
 
-//     fetch(`http://localhost:3000/lists/${listId}`, {
-//       method: "PATCH",
-//       headers: {
-//         'Content-Type': 'application/json',
-//         Accept: 'application/json'
-//       },
-//       body: JSON.stringify({
-//         completed: newState
-//       })
-//     })
-//     .then(resp => resp.json())
-//     .then(newList => {
+    fetch(`http://localhost:3000/lists/${listId}`, {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        completed: newState
+      })
+    })
+    .then(resp => resp.json())
+    // .then(function(json){
+    //     console.log(json)
+    //   })
+    .then(newList => {
 
-//       const newLists = foundCard.lists.map(list => {
-//         if (list.id === listId){
-//           return newList
-//         } else {
-//           return list
-//         }
-//       })
-//       foundCard.lists = newLists
+      const newLists = foundCard.lists.map(list => {
+        if (list.id === listId){
+          return newList
+        } else {
+          return list
+        }
+      })
+      foundCard.lists = newLists
 
 
-//       const newCards = this.state.cards.map(card => {
-//         if (card.id === cardId) {
-//           return foundCard
-//         } else {
-//           return card
-//         }
-//       })
+      const newCards = this.state.cards.map(card => {
+        if (card.id === cardId) {
+          return foundCard
+        } else {
+          return card
+        }
+      })
 
-//       this.setState({
-//         cards: newCards
-//       })
-//     })
-//   }
+      this.setState({
+        cards: newCards
+      })
+    })
+  }
 
 
   render(){
     return (
       <div className="main-container">
         {/* <ToDoCardContainer cards={this.state.cards} addList={this.addList} handleClickList={this.handleClickList}/> */}
-        <ToDoCardContainer cards={this.state.cards}/>
+        <ToDoCardContainer cards={this.state.cards} addList={this.addList} handleClickList={this.handleClickList}/>
         <CreateCard createNewCard={this.createNewCard}/>
       </div>
     )
