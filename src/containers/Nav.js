0aa88f0e9f61d8e5d4ffe2cd2a2b4bf722631778
Kristen Clunie <kcluniesa@yahoo.com>
2020-10-06 21:33:16 -0400

    add nav bar

---
+++ src/containers/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Nav () {
+    return(
+      <div className="nav">
+        {/* <img className="logo" src="../sample_logo.png" /> */}
+        <h1>ToDo List</h1>
+      </div>
+    )
+}
+
+export default Nav;
